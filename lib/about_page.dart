bbb939341010b4d1b0729be86da5611f948836bc
Untitled Programmer <blairlucas448@gmail.com>
2025-11-15 11:40:41 +0000

    Created basic about page

---
+++ lib/about_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          "About Us",
+          style: TextStyle(fontSize: 36, color: Colors.black),
+        ),
+        Text("""Welcome to the Union Shop!
+
+We’re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!
+
+All online purchases are available for delivery or instore collection!
+
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don’t hesitate to contact us at hello@upsu.net.
+
+Happy shopping!
+
+The Union Shop & Reception Team​​​​​""")
+      ],
+    );
+  }
+}
