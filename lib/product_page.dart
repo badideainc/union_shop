5c30bc34c97d09eec37dceaabb0bfd9d84a18752
Untitled Programmer <blairlucas448@gmail.com>
2025-11-15 13:03:54 +0000

    Replaced existing footer

--- lib/product_page.dart
+++ lib/product_page.dart
@@ -363,19 +363,7 @@ class ProductPage extends StatelessWidget {
             // ),
 
             // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Text(
-                'Placeholder Footer',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
+            const Footer(),
           ],
         ),
       ),
