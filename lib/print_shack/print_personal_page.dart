8a65e194577840c23050a24516e862e8af7d2ab1
Untitled Programmer <blairlucas448@gmail.com>
2025-11-16 13:07:05 +0000

    Added header features

--- lib/print_shack/print_personal_page.dart
+++ lib/print_shack/print_personal_page.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/print_shack/print_about_page.dart';
+import 'package:union_shop/main.dart ';
 
 class PrintPersonalisationPage extends StatelessWidget {
   const PrintPersonalisationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: SingleChildScrollView(
       child: Column(
         children: [
-          SizedBox(
+          Container(
+            height: 100,
+            color: Colors.white,
+            child: const Column(children: [
+              // Top banner
+              TopBanner(),
+              // Main header
+
+              NavBar(),
+            ]),
+          ),
+          const SizedBox(
               height: 250,
               child: PrintImage(
                   imageUrl:
                       "https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282")),
-          SizedBox(height: 20),
-          Text("Personalisation",
+          const SizedBox(height: 20),
+          const Text("Personalisation",
               style: TextStyle(fontSize: 36, color: Colors.black)),
         ],
       ),
