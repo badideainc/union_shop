0efdb1b6e42a72771766bb28f4a98828f9627dc6
Untitled Programmer <blairlucas448@gmail.com>
2025-11-16 13:05:46 +0000

    Added Image and heading

--- lib/print_shack/print_personal_page.dart
+++ lib/print_shack/print_personal_page.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:union_shop/print_shack/print_about_page.dart';
 
 class PrintPersonalisationPage extends StatelessWidget {
   const PrintPersonalisationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+              height: 250,
+              child: PrintImage(
+                  imageUrl:
+                      "https://shop.upsu.net/cdn/shop/files/PortsmouthCityMagnet1_1024x1024@2x.jpg?v=1752230282")),
+          SizedBox(height: 20),
+          Text("Personalisation",
+              style: TextStyle(fontSize: 36, color: Colors.black)),
+        ],
+      ),
+    ));
   }
 }
