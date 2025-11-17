4955a90ec437d0e6bfb37aee64093f5700863721
Untitled Programmer <blairlucas448@gmail.com>
2025-11-17 16:47:19 +0000

    Implemented productFromJson which will take an ID and fill in the correct information

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,22 +1,33 @@
+import 'package:union_shop/json_parser.dart';
+
 class ProductModel {
   final String id;
-  final String name;
-  final String description;
-  final String imageUrl;
+  String name = "";
+  String description = "";
+  String imageUrl = "";
 
-  final int quantity;
-  final int price;
+  int quantity = 0;
+  int price = 0;
 
   //Broad array for the different types of the item
-  final Map<String, List<String>>? options;
+  Map<String, List<String>>? options;
 
-  const ProductModel({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.imageUrl,
-    this.options,
-    this.quantity = 1,
-    required this.price,
-  });
+  ProductModel({required this.id});
+
+  static Future<ProductModel> productFromJson(String id) async {
+    {
+      final productList = await loadProductData();
+      for (final productData in productList) {
+        if (productData['id'] == id) {
+          final model = ProductModel(id: id);
+          model.name = productData['name'] ?? model.name;
+          model.description = productData['description'] ?? model.description;
+          model.imageUrl = productData['imageUrl'] ?? model.imageUrl;
+          model.price = productData['price'] ?? model.price;
+          return model;
+        }
+      }
+      return ProductModel(id: id);
+    }
+  }
 }
