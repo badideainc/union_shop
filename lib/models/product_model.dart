6cdbdaca08b49ce728ca84217add2cc63eb1fd72
Untitled Programmer <blairlucas448@gmail.com>
2025-11-18 12:31:59 +0000

    Made variables in ProductModel private

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -2,15 +2,14 @@ import 'package:union_shop/json_parser.dart';
 
 class ProductModel {
   final String id;
-  String name = "";
-  String description = "";
-  String imageUrl = "";
+  String _name = "";
+  String _description = "";
+  String _imageUrl = "";
 
-  int quantity = 0;
-  int price = 0;
-
+  int _quantity = 0;
+  int _price = 0;
   //Broad array for the different types of the item
-  Map<String, List<String>>? options;
+  Map<String, List<String>>? _options;
 
   ProductModel({required this.id});
 
@@ -20,10 +19,10 @@ class ProductModel {
       for (final productData in productList) {
         if (productData['id'] == id) {
           final model = ProductModel(id: id);
-          model.name = productData['name'] ?? model.name;
-          model.description = productData['description'] ?? model.description;
-          model.imageUrl = productData['imageUrl'] ?? model.imageUrl;
-          model.price = productData['price'] ?? model.price;
+          model._name = productData['name'] ?? model._name;
+          model._description = productData['description'] ?? model._description;
+          model._imageUrl = productData['imageUrl'] ?? model._imageUrl;
+          model._price = productData['price'] ?? model._price;
           return model;
         }
       }
@@ -32,10 +31,10 @@ class ProductModel {
   }
 
   String get productId => id;
-  String get productName => name;
-  String get productDescription => description;
-  String get productImageUrl => imageUrl;
-  int get productPrice => price;
-  int get productQuantity => quantity;
-  Map<String, List<String>>? get productOptions => options;
+  String get productName => _name;
+  String get productDescription => _description;
+  String get productImageUrl => _imageUrl;
+  int get productPrice => _price;
+  int get productQuantity => _quantity;
+  Map<String, List<String>>? get productOptions => _options;
 }
