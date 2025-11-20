634ee696e7de3c16a20e7d7abf93554369e66c98
Untitled Programmer <blairlucas448@gmail.com>
2025-11-20 15:46:32 +0000

    Added basic functions to cart

---
+++ lib/models/cart_model.dart
@@ -0,0 +1,19 @@
+import 'package:union_shop/models/product_model.dart';
+
+class CartModel {
+  List<ProductModel> _items = [];
+
+  void addItem(ProductModel product) {
+    _items.add(product);
+  }
+
+  void removeItem(ProductModel product) {
+    _items.remove(product);
+  }
+
+  void clearCart() {
+    _items.clear();
+  }
+
+  List<ProductModel> get items => _items;
+}
