84f3689fba709b9444a83ed0140f2708b0d91275
Untitled Programmer <blairlucas448@gmail.com>
2025-11-20 13:42:39 +0000

    Created CollectionPage stateful widget and _CollectionPageState skeletons

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -1,1 +1,21 @@
-class CollectionPage {}
+import 'package:flutter/material.dart';
+import 'package:union_shop/models/category.dart';
+
+class CollectionPage extends StatefulWidget {
+  final Category category;
+
+  const CollectionPage({super.key, required this.category});
+
+  @override
+  State<CollectionPage> createState() => _CollectionPageState();
+}
+
+class _CollectionPageState extends State<CollectionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(categoryTitle(widget.category))),
+      body: const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
