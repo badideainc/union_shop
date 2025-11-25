9d408598e374e4b0ff4f11d26c02d9ccc48f9027
Untitled Programmer <blairlucas448@gmail.com>
2025-11-25 12:07:41 +0000

    Added heading elements to page

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/main.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,10 +11,19 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Basket'),
-      ),
-    );
+    return Scaffold(
+        body: Column(
+      children: [
+        const Header(),
+        Text(
+          'Your Cart',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        const Text(
+          'Continue shopping',
+          style: TextStyle(color: Color(0xFF4d2963)),
+        ),
+      ],
+    ));
   }
 }
