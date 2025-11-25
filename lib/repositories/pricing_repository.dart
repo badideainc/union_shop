58f2d065023e882e922cd0377f5cc5e2af34b445
Untitled Programmer <blairlucas448@gmail.com>
2025-11-25 09:40:52 +0000

    Created method to calculate total price

--- lib/repositories/pricing_repository.dart
+++ lib/repositories/pricing_repository.dart
@@ -1,7 +1,12 @@
 import "package:union_shop/models/product_model.dart";
 
 class PricingRepository {
-  double calculatePrice(List<ProductModel> products) {
-    return 0;
+  double calculateTotalPrice(List<ProductModel> products) {
+    double price = 0.0;
+
+    for (ProductModel product in products) {
+      price += product.price;
+    }
+    return price;
   }
 }
