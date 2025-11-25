f8c079f04bd85f1e97138770a10f32efd1bade82
Untitled Programmer <blairlucas448@gmail.com>
2025-11-25 09:41:59 +0000

    Added handling for empty list

--- lib/repositories/pricing_repository.dart
+++ lib/repositories/pricing_repository.dart
@@ -2,6 +2,10 @@ import "package:union_shop/models/product_model.dart";
 
 class PricingRepository {
   double calculateTotalPrice(List<ProductModel> products) {
+    if (products.isEmpty) {
+      return 0.0;
+    }
+
     double price = 0.0;
 
     for (ProductModel product in products) {
