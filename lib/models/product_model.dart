e5b652c8dfe3e573e85c409181dbe3985ef6bef2
Untitled Programmer <blairlucas448@gmail.com>
2025-11-28 15:25:08 +0000

    Add selected options management to ProductModel for UI persistence

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -14,6 +14,8 @@ class ProductModel {
   double _salePrice = -1;
   //Broad array for the different types of the item
   Map<String, List<String>>? _options;
+  // Store the current selection for each option key (e.g. "Size": "M").
+  Map<String, String>? _selectedOptions;
 
   ProductModel();
 
@@ -50,6 +52,8 @@ class ProductModel {
                 return MapEntry(key.toString(), [value?.toString() ?? '']);
               }
             });
+            // Initialise selected options map so UI can write selections
+            model._selectedOptions = {};
           }
           model._salePrice = productData['salePrice'] ?? model._salePrice;
           return model;
@@ -71,11 +75,18 @@ class ProductModel {
 
   int get quantity => _quantity;
   Map<String, List<String>>? get options => _options;
+  Map<String, String>? get selectedOptions => _selectedOptions;
 
   // Mutators for quantity so other code can update product quantity when
   // the product instance is used as a cart item.
   void setQuantity(int q) {
     _quantity = q;
+  }
+
+  /// Set a selection for an option key.
+  void setSelectedOption(String key, String value) {
+    _selectedOptions ??= {};
+    _selectedOptions![key] = value;
   }
 
   void incrementQuantity([int delta = 1]) {
@@ -98,5 +109,8 @@ class ProductModel {
     target._imageUrl = _imageUrl;
     target._category = _category;
     target._price = _price;
+    // Copy selected options if present so personalised selections persist
+    target._selectedOptions =
+        _selectedOptions == null ? null : Map.from(_selectedOptions!);
   }
 }
