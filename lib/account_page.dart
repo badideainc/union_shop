aed83a449d2b133bebff7acdd8622afe9d1717f2
Untitled Programmer <blairlucas448@gmail.com>
2025-11-28 12:37:29 +0000

    Add initial implementation of AccountPage widget

---
+++ lib/account_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account Page'),
+      ),
+      body: const Center(
+        child: Text('This is the account page.'),
+      ),
+    );
+  }
+}
