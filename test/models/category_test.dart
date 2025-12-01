93be5ad2f0db0ffc9f86b99c2229135aa932c128
Untitled Programmer <blairlucas448@gmail.com>
2025-12-01 11:59:19 +0000

    Add tests for categoryFromString function in CategoryModel

--- test/models/category_test.dart
+++ test/models/category_test.dart
@@ -18,4 +18,27 @@ void main() {
       expect(categoryTitle(ProductCategory.sale), 'SALE!');
     });
   });
+
+  group('Category - parsing', () {
+    test('categoryFromString parses known enum-style strings', () {
+      expect(categoryFromString('clothing'), ProductCategory.clothing);
+      expect(categoryFromString('merchandise'), ProductCategory.merchandise);
+      expect(categoryFromString('halloween'), ProductCategory.halloween);
+      expect(categoryFromString('signatureAndEssentialsRange'),
+          ProductCategory.signatureAndEssentialsRange);
+      expect(categoryFromString('portsmouthCityCollection'),
+          ProductCategory.portsmouthCityCollection);
+      expect(categoryFromString('prideCollection'),
+          ProductCategory.prideCollection);
+      expect(categoryFromString('graduation'), ProductCategory.graduation);
+      expect(categoryFromString('personalised'), ProductCategory.personalised);
+      expect(categoryFromString('sale'), ProductCategory.sale);
+    });
+
+    test('categoryFromString returns null for unknown strings', () {
+      expect(categoryFromString('not-a-category'), isNull);
+      expect(categoryFromString(''), isNull);
+      expect(categoryFromString(null), isNull);
+    });
+  });
 }
