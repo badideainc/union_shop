9e8ef72d56917710de6a85ec123ff4b7acf821e7
Untitled Programmer <blairlucas448@gmail.com>
2025-12-01 13:46:32 +0000

    Add test for AccountPage to verify disabled state of 'Sign in with shop' button

--- test/account_test.dart
+++ test/account_test.dart
@@ -15,4 +15,16 @@ void main() {
     expect(find.text('The UNION'), findsOneWidget);
     expect(find.text('Sign in'), findsOneWidget);
   });
+
+  testWidgets('Sign in with shop button exists and is disabled',
+      (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: AccountPage()));
+    await tester.pumpAndSettle();
+
+    final finder = find.widgetWithText(ElevatedButton, 'Sign in with shop');
+    expect(finder, findsOneWidget);
+
+    final ElevatedButton button = tester.widget<ElevatedButton>(finder);
+    expect(button.onPressed, isNull);
+  });
 }
